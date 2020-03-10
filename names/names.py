a89ffefc0c3bdf7199a896fb9fd3098527e18d49
umbur <you@example.com>
2020-03-10 15:48:45 -0600

    adds python sets solution

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from BinarySearchTree import BinarySearchTree
 
 start_time = time.time()
 
@@ -16,25 +17,25 @@ f.close()
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+# print(duplicates)
 
 # New solution to reduce runtime:
-# Defineing storage which is an object and will act as a cache
-storage = {}
 # Defining duplicates array which should store the duplicated values
+# The python sets solution:
 duplicates = []
+name2 = set(names_2)
+for value in names_1:
+    if value in name2:
+        duplicates.append(value)
+# print(duplicates)
+# bst = BinarySearchTree(names_1[0])
+# for i in range(1, len(names_1)-1):
+# #     print(len(names_1))
+#     bst.insert(names_1[i])
 
-# Looping through the values of names_1
-for name_1 in names_1:
-    # Assigning values of names_1 to the storage object
-    storage[name_1] = name_1
-# Looping through the values of names_2
-for name_2 in names_2:
-# If the values in storage equals values in names_2
-        if name_1 in storage == name_2:
-# Then append those values to the duplicates array
-            duplicates.append(name_1)
-print(duplicates)
-
+# for name in names_2:
+#     if (bst.contains(name)):
+#         duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
